3e34c63736a0ac87eeaadd4fdca5e15d82600207
Mayara Silva <devmayara11@gmail.com>
2022-06-04 19:41:03 -0300

    listando db na pagina sistema

--- sistema.php
+++ sistema.php
@@ -11,6 +11,12 @@ if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['password']) == tr
 }
 $logado = $_SESSION['email'];
 
+$sql = "SELECT * FROM usuarios ORDER BY id DESC";
+
+$result = $conexao->query($sql);
+
+print_r($result);
+
 ?>
 
 <!DOCTYPE html>
@@ -46,6 +52,44 @@ $logado = $_SESSION['email'];
                 echo "<h1>Bem-Vindo! <u>$logado</u></h1>";
             ?>
             <h1>Você acessou o sistema</h1>
+
+            <table class="table mt-5">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Senha</th>
+                        <th>Telefone</th>
+                        <th>Sexo</th>
+                        <th>Data de Nascimento</th>
+                        <th>Cidade</th>
+                        <th>Estado</th>
+                        <th>Endereço</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                        while($user_data = mysqli_fetch_assoc($result))
+                        {
+                            echo "<tr>";
+                            echo "<td>".$user_data['id']."</td>";
+                            echo "<td>".$user_data['name']."</td>";
+                            echo "<td>".$user_data['email']."</td>";
+                            echo "<td>".$user_data['password']."</td>";
+                            echo "<td>".$user_data['phone']."</td>";
+                            echo "<td>".$user_data['sexo']."</td>";
+                            echo "<td>".$user_data['data_nasc']."</td>";
+                            echo "<td>".$user_data['cidade']."</td>";
+                            echo "<td>".$user_data['estado']."</td>";
+                            echo "<td>".$user_data['endereco']."</td>";
+                            echo "<td>Ações</td>";
+                            echo "</td>";
+                        }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </section>
 </body>
